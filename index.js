53bd8e73533df65ac0d779dd629c6c4e7bd6f201
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-25 00:06:02 +0600

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m7jtw.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database connected');
+        const serviceCollection = client.db('parts_manufacturer').collection('product');
+
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello Bike Manufacture!')
