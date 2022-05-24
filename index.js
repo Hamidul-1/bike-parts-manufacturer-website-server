eaebed84213669094d3343d4afd05489d2a797e3
Hamidul-1 <hamidul786cse@gmail.com>
2022-05-24 22:57:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m7jtw.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+app.get('/', (req, res) => {
+    res.send('Hello Bike Manufacture!')
+})
+
+app.listen(port, () => {
+    console.log(`Bike Manufacture app listening on port ${port}`)
+})
